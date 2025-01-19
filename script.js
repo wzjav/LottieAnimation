74445df2878306c7d7c92e7d0a8b4d8ba1d6fa0d
wzjav <s10269031@connect.np.edu.sg>
2025-01-19 17:48:55 +0800

    DOUBLE CHUNK CHOCOLATE COOKIE

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('Need to wait');
+
+setTimeout(function() {
+  console.log('setting window location');
+  window.location.href = "https://www.tiktok.com/@a.j.befumo/video/7400871854958808363?lang=en";
+}, 60000);
